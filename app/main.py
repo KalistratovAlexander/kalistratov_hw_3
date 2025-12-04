8001eed3e775b4acd367d6c402bfd6a440001e4c
a.s.kalistratov <aleksandr.kalistratov@diginetica.com>
2025-12-04 19:18:35 +0300

    First version

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# app/main.py
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+VERSION = os.getenv("MODEL_VERSION", "v1.0.0")
+
+@app.get("/health")
+def health():
+    return {"status": "ok", "version": VERSION}
+
+@app.get("/predict")
+def predict():
+    return {"prediction": "ok", "version": VERSION}
